e044e20f87a41f4381de505cd846350af79e6f37
Karim Helaly <69760092+Kimo-boop@users.noreply.github.com>
2023-07-01 22:53:58 +0200

    Update one.java

--- src/one.java
+++ src/one.java
@@ -1,3 +1,10 @@
 public class one {
+    public static void main(String[] args) {
+    int x; //declaration
+    x = 1; //assignment
 
+    int y = 1234; //initialization
+
+    System.out.println("My number is; "+y);
+}
 }
