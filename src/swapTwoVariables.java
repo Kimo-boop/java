e58e3e5a1aec9647795ffade2599fce74d460edd
Karim Helaly <69760092+Kimo-boop@users.noreply.github.com>
2023-07-02 13:25:43 +0200

    Create swapTwoVariables.java

---
+++ src/swapTwoVariables.java
@@ -0,0 +1,10 @@
+public class swapTwoVariables {
+    public static void main(String[] args) {
+        String x = "water";
+        String y = "Kool-Aid";
+        String temp = null;
+
+        System.out.println("x:"+x);
+        System.out.println("y:"+y);
+    }
+}
