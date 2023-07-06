d09e23d7a7710d20554744e8565397ff67f8c0a8
Karim Helaly <69760092+Kimo-boop@users.noreply.github.com>
2023-07-06 17:44:33 +0200

    Create expressions.java

---
+++ src/expressions.java
@@ -0,0 +1,12 @@
+public class expressions {
+    public static void main(String[]args){
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
+        double friends = 10;
+
+        friends = (double) friends / 3 ;
+
+        System.out.println(friends);
+    }
+}
