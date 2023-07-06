25772798b2a3d567a5e62a126cadf09a83a22a94
Karim Helaly <69760092+Kimo-boop@users.noreply.github.com>
2023-07-06 17:32:21 +0200

    Update userInput.java

--- src/userInput.java
+++ src/userInput.java
@@ -6,8 +6,15 @@ public class userInput {
 
         System.out.println("What is your Name?");
         String name = scanner.nextLine();
+        System.out.println("How old are you");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food");
+        String food = scanner.nextLine();
 
         System.out.println("Hello "+name);
+        System.out.println("You are "+ age +" years old");
+        System.out.println("and you like "+food);
     }
 
 
